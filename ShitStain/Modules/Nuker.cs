6fc157683cb9b1704bd26d9abf8ec37fc1fb722e
notcarlton <carltonrbx@outlook.com>
2021-10-12 12:46:11 -0600

    made nuker activate on left click

--- ShitStain/Modules/Nuker.cs
+++ ShitStain/Modules/Nuker.cs
@@ -16,15 +16,17 @@ namespace ShitStain.Modules
 
         public override void onUpdate()
         {
-            Hitable[] objects = UnityEngine.Object.FindObjectsOfType(typeof(Hitable)) as Hitable[];
-            foreach (Hitable obj in objects) {
-                PlayerMovement movement = Object.FindObjectOfType(typeof(PlayerMovement)) as PlayerMovement;
-                Vector3 pos = movement.transform.position;
+            if (Input.GetButtonDown("Fire1")) {
+                Hitable[] objects = UnityEngine.Object.FindObjectsOfType(typeof(Hitable)) as Hitable[];
+                foreach (Hitable obj in objects) {
+                    PlayerMovement movement = Object.FindObjectOfType(typeof(PlayerMovement)) as PlayerMovement;
+                    Vector3 pos = movement.transform.position;
 
-                Vector3 distVector = pos - obj.transform.position;
+                    Vector3 distVector = pos - obj.transform.position;
 
-                if (distVector.sqrMagnitude < 500) 
-                    ClientSend.PlayerHitObject(1000, obj.GetId(), 3, new Vector3(0, 0, 0), 3);
+                    if (distVector.sqrMagnitude < 500)
+                        ClientSend.PlayerHitObject(1000, obj.GetId(), 3, new Vector3(0, 0, 0), 3);
+                }
             }
         }
     }
